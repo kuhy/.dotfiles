ddd3d9558b8b5735226a1049b81b44dc9b3da70d
Ondřej Kuhejda <ondrejkuhejda@gmail.com>
2019-03-17 02:33:46 +0100

    Remake install.sh

--- install.sh
+++ install.sh
@@ -1,51 +1,29 @@
 #! /bin/bash
 # Author: Kuhy
-# Stolen from: Aaron Kuehler
-# Purpose: Install the .dotfiles
+# Installs and tangles literate dotfiles written in Org mode.
 
+# Directory where will be .dotfiles installed.
 DOTFILES_HOME=$HOME/.dotfiles
 
-function clone_or_update_repo() {
-    if [ -e $DOTFILES_HOME ]; then
-        echo "Updating $DOTFILES_HOME"
-        cd $DOTFILES_HOME
-        git pull
-    else
-        echo "Installing dotfiles to: $DOTFILES_HOME"
-        git clone git@github.com:kuhy/.dotfiles.git $DOTFILES_HOME
-        cd $DOTFILES_HOME
-    fi
-}
+# Clones .dotfiles from GitHub if folder with dotfiles doesn't exist
+# and installs all project dependencies.
+if [ ! -e $DOTFILES_HOME ]; then
+    echo "Installing dependencies..."
+    sudo dnf install emacs git
+    echo "Installing dotfiles to $DOTFILES_HOME."
+    git clone git@github.com:kuhy/.dotfiles.git $DOTFILES_HOME
+fi
 
-function tangle_files() {
-    DIR=`pwd`
-    FILES=""
+cd $DOTFILES_HOME
 
-    for file in `ls -1 | grep \.org | grep -v README.org`; do
-        FILES="$FILES \"$file\""
-    done
-
-    echo -e "Installing: \n$FILES"
-    emacs -Q --batch --eval \
-        "(progn
-            (require 'org)
-            (require 'ob)
-            (require 'ob-tangle)
-            (mapc
-                (lambda
-                    (file)
-                    (find-file
-                        (expand-file-name file \"$DIR\")
-                    )
-                    (org-babel-tangle)
-                    (kill-buffer)
-                )
-                '($FILES)
-            )
-        )"
-}
-
-clone_or_update_repo
-tangle_files
+# Exports and then tangles Org files in directory.
+# Exporting is needed because of #+INCLUDE isn't executed during tangling.
+# Omits README.org and colors.org.
+for file in `ls -1 | awk '/\..*org$/ && !/README.org/ && !/colors.org/'`; do
+    emacs $file -Q --batch -f org-org-export-to-org -kill
+    emacs $file.org -Q --batch -f org-babel-tangle --kill
+    rm $file.org
+done
 
 exit 0
+
