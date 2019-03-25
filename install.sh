c29d57b411a064504faf6b8d536a6d1067dad83c
Ondřej Kuhejda <ondrejkuhejda@gmail.com>
2019-03-26 00:42:07 +0100

    Add termite

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ cd $DOTFILES_HOME
 # Exports and then tangles Org files in directory.
 # Exporting is needed because of #+INCLUDE isn't executed during tangling.
 # Omits README.org and colors.org.
-for file in `ls -1 | awk '/\..*org$/ && !/README.org/ && !/colors.org/'`; do
+for file in `ls -1 | awk '/\..*org$/ && !/README.org/ && !/variables.org/'`; do
     emacs $file -Q --batch -f org-org-export-to-org -kill
     emacs $file.org -Q --batch -f org-babel-tangle --kill
     rm $file.org
