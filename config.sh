a0d74a1339b35107f93f40651a8e6f36243f6ac6
Ondřej Kuhejda <ondrejkuhejda@gmail.com>
2019-08-20 20:33:23 +0200

    add dependencies installation to config.sh

--- config.sh
+++ config.sh
@@ -3,5 +3,35 @@
 # Setups system after new installation.
 # TODO: make Org file from this
 
-xdg-settings set default-web-browser qutebrowser.desktop
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+# enable copr repos
+dnf -y copr enable gumieri/sway
+dnf -y copr enable wyvie/i3blocks
+dnf -y copr enable skidnik/termite
+
+# install packages
+  # tiling window manager
+  # feed generator for text based status bars
+  # keyboard-centric terminal
+  # friendly interactive shell
+  # web browser
+  # window switcher, application launcher and dmenu replacement
+  # terminal multiplexer
+  # screen locker
+dnf -y install \
+  sway \
+  i3blocks \
+  termite \
+  fish \
+  firefox \
+  rofi \
+  tmux \
+  swaylock
+
+# set default browser
+xdg-settings set default-web-browser firefox.desktop
 
