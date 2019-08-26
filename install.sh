a55e9aa811879ac6e6c74d5d00f948f5c7f004b5
Ondřej Kuhejda <ondrejkuhejda@gmail.com>
2019-08-27 00:43:28 +0200

    Improve tangling

--- install.sh
+++ install.sh
@@ -7,23 +7,37 @@ DOTFILES_HOME=$HOME/.dotfiles
 
 # Clones .dotfiles from GitHub if folder with dotfiles doesn't exist
 # and installs all project dependencies.
-if [ ! -e $DOTFILES_HOME ]; then
+if [ ! -e "$DOTFILES_HOME" ]; then
     echo "Installing dependencies..."
     sudo dnf install emacs git
     echo "Installing dotfiles to $DOTFILES_HOME."
-    git clone https://github.com/kuhy/.dotfiles.git $DOTFILES_HOME
+    git clone https://github.com/kuhy/.dotfiles.git "$DOTFILES_HOME"
 fi
 
-cd $DOTFILES_HOME
+cd "$DOTFILES_HOME" || exit
 
-# Exports and then tangles Org files inside a directory.
+emacs_evaluate() {
+    emacs -Q --batch --eval "
+      (dolist (file command-line-args-left)
+        (with-current-buffer
+          (find-file-noselect file)
+          ($1)
+        )
+      )" $FILES
+}
+
+# Exports and then tangles Org files inside a specified directory.
 # Exporting is needed because #+INCLUDE isn't executed during tangling.
 # Omits README.org and colors.org.
-for file in `find -mindepth 2 -maxdepth 2 -name "*.org"`; do
-    emacs $file -Q --batch -f org-org-export-to-org -kill
-    emacs $file.org -Q --batch -f org-babel-tangle --kill
-    rm $file.org
-done
+FILES=$(find "$DOTFILES_HOME/$*" -path "$DOTFILES_HOME/[!.]*/*.org")
+
+emacs_evaluate 'org-org-export-to-org'
+
+FILES=$(echo "$FILES" | sed 's/$/.org/')
+
+emacs_evaluate 'org-babel-tangle'
+
+rm $FILES
 
 exit 0
 
